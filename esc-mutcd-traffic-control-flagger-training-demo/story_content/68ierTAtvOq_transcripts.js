b393e58f7c7b95c771a77dfe47856a6758a21c9d
Jarrett Allen Young <young.jarrett@gmail.com>
2026-01-13 11:31:39 -0800

    Add demo folders

---
+++ esc-mutcd-traffic-control-flagger-training-demo/story_content/68ierTAtvOq_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":544,"text":"If you are flagging near a turn,"},{"start":2880,"text":"be aware that larger vehicles such as trucks, RVs, and buses, may take up part of the shoulder as they turn."},{"start":11392,"text":"Depending on the vehicle, this can be true for both right and left turns."},{"start":17568,"text":"Make sure you have a route of escape that allows you to move out of their way."}]}]};
+    window.globalLoadJsAsset('story_content/68ierTAtvOq_transcripts.js', JSON.stringify(data));
+})();
---
+++ eversafe-driving-program-demo/story_content/6nEg2zbb7tL_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":448,"text":"What vehicle do you drive the most?"},{"start":3328,"text":"Is it a company-owned vehicle, or yours?"},{"start":6688,"text":"Do you drive the same vehicle every day, or switch between different ones?"},{"start":11520,"text":"No matter what vehicle you use,"},{"start":13629,"text":"before you drive, you'll want to make sure you understand all of its features,"},{"start":17892,"text":" and check that it's in good shape."}]}]};
+    window.globalLoadJsAsset('story_content/6nEg2zbb7tL_transcripts.js', JSON.stringify(data));
+})();
